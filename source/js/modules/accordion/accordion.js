5ec4e156bd8c5597dc71c241145b7d566d8ff541
Kungfu-Chsia <allrighty@ya.ru>
2022-05-18 22:27:50 +0300

    правки итог

--- source/js/modules/accordion/accordion.js
+++ source/js/modules/accordion/accordion.js
@@ -9,16 +9,25 @@ const initAccordion = function () {
 
   closeAccordion();
 
+
   accordionEls.forEach(function (accordionEl) {
     const togglewrapperEl = accordionEl.querySelector('.accordion__toggle-wrapper');
+    const togglegorizEl = accordionEl.querySelector('.accordion__toggle-gorizont');
+    const togglevertEl = accordionEl.querySelector('.accordion__toggle-vert');
+
+
+    togglegorizEl.classList.remove('hidden');
+    togglegorizEl.classList.add('active');
+    togglevertEl.classList.add('active');
 
     togglewrapperEl.addEventListener('click', function () {
-
       if (accordionEl.classList.contains('accordion--show')) {
         closeAccordion();
+        togglevertEl.classList.toggle('active');
       } else {
         closeAccordion();
         accordionEl.classList.add('accordion--show');
+        togglevertEl.classList.toggle('active');
       }
 
 
